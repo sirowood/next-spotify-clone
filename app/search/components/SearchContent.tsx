23367cfd31cbc53dd3187859fc8243c6c19014f0
Xuefeng Wu <sirowood@gmail.com>
2023-08-09 15:52:52 +0300

    play song function

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -2,6 +2,7 @@
 
 import MediaItem from '@/components/MediaItem';
 import LikeButton from '@/components/LikeButton';
+import useOnPlay from '@/hooks/useOnPlay';
 import { Song } from '@/types';
 
 interface SearchContentProps {
@@ -9,6 +10,8 @@ interface SearchContentProps {
 }
 
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return (
       <div
@@ -35,7 +38,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className="flex-1">
             <MediaItem
               data={song}
-              onClick={() => {}}
+              onClick={(id: string) => onPlay(id)}
             />
           </div>
           <LikeButton songId={song.id} />
--- components/Library.tsx
+++ components/Library.tsx
@@ -2,9 +2,11 @@
 
 import { TbPlaylist } from 'react-icons/tb';
 import { AiOutlinePlus } from 'react-icons/ai';
+
 import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUoloadModal';
+import useOnPlay from '@/hooks/useOnPlay';
 import MediaItem from './MediaItem';
 import { Song } from '@/types';
 
@@ -16,6 +18,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
+
+  const onPlay = useOnPlay(songs);
 
   const onClick = () => {
     if (!user) {
@@ -46,7 +50,7 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
           <MediaItem
-            onClick={() => {}}
+            onClick={(id: string) => onPlay(id)}
             key={item.id}
             data={item}
           />
